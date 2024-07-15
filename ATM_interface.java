5694166178bc539a4009b7bfeef265cc477684ff
NAVEEN-R-SN <naveennadiu050@gmail.com>
2024-07-15 10:30:08 +0530

    Create ATM_interface.java

---
+++ ATM_interface.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid amount for deposit.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+        } else {
+            System.out.println("Withdrawal failed. Insufficient balance.");
+        }
+    }
+}
