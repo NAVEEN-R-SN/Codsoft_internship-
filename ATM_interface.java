3c057595e2da608a91451aa4f296aeee198da98e
NAVEEN-R-SN <naveennadiu050@gmail.com>
2024-07-16 21:32:59 +0530

    Update ATM_interface.java

--- ATM_interface.java
+++ ATM_interface.java
@@ -1,29 +1,55 @@
-public class BankAccount {
-    private double balance;
+import java.util.*;
 
-    public BankAccount(double initialBalance) {
-        this.balance = initialBalance;
+public class atm{
+    private bank_account account;
+
+    public atm(bank_account account){
+        this.account = account;
     }
 
-    public double getBalance() {
-        return balance;
+    public void displayMenu(){
+        System.out.println("\nATM Menu:");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
     }
 
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposit successful. New balance: $" + balance);
-        } else {
-            System.out.println("Invalid amount for deposit.");
+    public void performAction(int choice){
+        Scanner sc=new Scanner(System.in);
+        switch(choice){
+            case 1:
+                checkBalance();
+                break;
+            case 2:
+                System.out.print("Enter the amount to be  deposit: ");
+                int Damount = sc.nextInt();
+                deposit(Damount);
+                break;
+            case 3:
+                System.out.print("Enter the amount to be withdraw: ");
+                int Wamount=sc.nextInt();
+                withdraw(Wamount);
+                break;
+            case 4:
+                System.out.println("Exiting...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
         }
+        
     }
 
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrawal successful. New balance: $" + balance);
-        } else {
-            System.out.println("Withdrawal failed. Insufficient balance.");
-        }
+    public void checkBalance(){
+        System.out.println("\nCurrent balance :" + account.getBalance());
+    }
+
+    public void deposit(int Damount){
+        account.deposit(Damount);
+    }
+
+    public void withdraw(int Wamount){
+        account.withdraw(Wamount);
     }
 }
